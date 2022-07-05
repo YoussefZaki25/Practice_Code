8035b85c20f9e263502af89f108c6412b799b99d
Christian Velez <your.friendly.neighborhood.dev@gmail.com>
2022-07-05 11:46:43 -0700

    Update count_to_ten.py
    Added a clarification here and gave the answer for how to complete this using range.

--- Algorithims/count_to_ten.py
+++ Algorithims/count_to_ten.py
@@ -6,19 +6,18 @@
 for x in range(0,10):
     print(x)
 
+#So you almost have the right idea here.  Let me show you a little example of how I want you to complete these challenges
 
+#First declare the function by what its supposed to be
+def count_to_ten():
+    
+    #Next write a for loop or a while loop to execute the function.
+    #You can use range since its a function python gives you but there are other functions 
+    #you will want to use a for loop instead.
 
-
-
-
-
-
-
-
-
-
-
-
+    #Using range this is what the function would look like
+    for number in range(1,11):
+        print(number)
 
 
 
