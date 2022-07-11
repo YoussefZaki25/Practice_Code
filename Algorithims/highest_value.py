f754d8c208e51c3eec3bd589f89280bef5c1de34
Christian Velez <your.friendly.neighborhood.dev@gmail.com>
2022-07-11 12:39:47 -0700

    Created new challenge
    The new challenge is to loop through the items in the list and return the highest value.

---
+++ Algorithims/highest_value.py
@@ -0,0 +1,8 @@
+''' 
+In this challenge I want you to go through the list and return the index of the list that has the highest numbered value.  Dont print the index list directly. Find it
+using code (ie for loops and conditionals)
+'''
+numbers = [75, 89, 23, 77, 90, 1]
+
+# Write your code within the function
+def highestVal():
